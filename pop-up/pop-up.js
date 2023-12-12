f1311ce03e05a1252031e12a0aa473b1b7b15853
Priyanshu <priyanshurai271@gmail.com>
2023-12-12 20:15:00 +0530

    feat: added pop up component

---
+++ pop-up/pop-up.js
@@ -0,0 +1,9 @@
+let dialog = document.getElementById('colorModal');
+
+function openPopUp() {
+    dialog.style.display = 'block';
+}
+
+function closePopUp() {
+    dialog.style.display = 'none';
+}
